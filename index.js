1ee2d0888c5a41175e848b876c47aae4a150fdf0
murlisoni <murlimanohar.soni@octalinfosolution.com>
2023-02-07 17:43:18 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+mongoose.set("strictQuery", false);
+const categoryRoute = require("./routes/category");
+var multer = require("multer");
+const path = require("path");
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.use(express.static("public"));
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "public/images"));
+  },
+  filename: function (req, file, cb) {
+    console.log("file", file);
+    let extenstionArray = file.mimetype.split("/");
+    let extention = extenstionArray[extenstionArray.length - 1] || "jpg";
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + "." + extention);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  console.log("req.file", req.file);
+  console.log("req.files", req.files);
+  res
+    .status(200)
+    .json({ message: "File has been uploaded", filename: req.file.filename });
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
