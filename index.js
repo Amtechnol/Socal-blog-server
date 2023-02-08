797286cbd9627edeb5c79ec28ff955e9b03515fe
murlisoni <murlimanohar.soni@octalinfosolution.com>
2023-02-08 12:14:07 +0530

    port-changes1

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen(process.env.PORT, () => {
+app.listen(5000, () => {
   console.log("Backend is running.");
 });
