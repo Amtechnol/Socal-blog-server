0ef6162154efeb85cd5aac125db351d0ab234fc7
murlisoni <murlimanohar.soni@octalinfosolution.com>
2023-02-08 10:43:22 +0530

    changes

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ const path = require("path");
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+
+const MONGO_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qdspt6i.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useFindAndModify: true,
