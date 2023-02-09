efcb46ef5ce63fde91b94381c851711aa67ba0f6
murlisoni <murlimanohar.soni@octalinfosolution.com>
2023-02-09 10:19:15 +0530

    change-json-to-send

--- routes/auth.js
+++ routes/auth.js
@@ -29,15 +29,15 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong credentials!");
+    !user && res.status(400).send("Wrong credentials!");
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong credentials!");
+    !validated && res.status(400).send("Wrong credentials!");
 
     const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    return res.status(200).json(others);
   } catch (err) {
-    res.status(500).json(err);
+    return res.status(500).json(err);
   }
 });
 
