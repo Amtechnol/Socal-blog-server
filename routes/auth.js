1e9e66a48dd154a1f2ac955c799c5c2bb786bc07
murlisoni <murlimanohar.soni@octalinfosolution.com>
2023-02-10 15:45:14 +0530

    profile-error-fix

--- routes/auth.js
+++ routes/auth.js
@@ -18,6 +18,9 @@ router.post("/register", async (req, res) => {
 
     // const user = await newUser.save();
 
+    newUser.profilePic = `https://murli-server.up.railway.app/images/${newUser.profilePic}`;
+    delete newUser.password;
+
     console.log("newuser", newUser);
     res.status(200).json(newUser);
   } catch (err) {
@@ -28,14 +31,14 @@ router.post("/register", async (req, res) => {
 //LOGIN
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username });
+    const user = await User.findOne({ username: req.body.username }).lean();
     !user && res.status(400).json("Wrong credentials!");
 
     const validated = await bcrypt.compare(req.body.password, user.password);
     !validated && res.status(400).json("Wrong credentials!");
-
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
+    user.profilePic = `https://murli-server.up.railway.app/images/${user.profilePic}`;
+    delete user.password;
+    res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
